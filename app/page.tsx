1090f4fa824b7223121ea6fa8ff3b7d5491b50cc
Dominik Koch <dominik@koch-bautechnik.de>
2025-04-13 17:55:17 +0200

    fix: change heading text

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,16 @@ import {
 	DiscordMessageGroup
 } from "@/registry/new-york/discord-base/discord-base";
 
-// This page displays items from the custom registry.
-// You are free to implement this with your own design as needed.
-
 export default function Home() {
 	return (
 		<div className="mx-auto flex min-h-svh max-w-3xl flex-col gap-8 px-4 py-8">
 			<header className="flex flex-col gap-1">
-				<h1 className="font-bold text-3xl tracking-tight">Custom Registry</h1>
+				<h1 className="font-bold text-3xl tracking-tight">
+					Discord Components
+				</h1>
 				<p className="text-muted-foreground">
-					A custom registry for distribution code using shadcn.
+					A collection of discord components for integrating discord like UI
+					into your project.
 				</p>
 			</header>
 			<main className="flex flex-1 flex-col gap-8">
