8b7aab6585fd70fabbd0ad91762809dfb6c3d758
Dominik Koch <dominik@koch-bautechnik.de>
2025-04-13 18:57:48 +0200

    build: add docker support

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	output: "standalone",
 	outputFileTracingIncludes: {
 		registry: ["./registry/**/*"]
 	}
-	/* config options here */
 };
 
 export default nextConfig;
